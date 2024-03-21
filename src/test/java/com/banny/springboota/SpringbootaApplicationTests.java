ad35799c58ca6320f8cc848ea29ee0ade3c54e21
BanseokSuh <still3028@gmail.com>
2024-03-21 20:52:49 +0900

    Feat: First commit
    First Commit
    Set server could run without spring security and spring jpa configuration

---
+++ src/test/java/com/banny/springboota/SpringbootaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banny.springboota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

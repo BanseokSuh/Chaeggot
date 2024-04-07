6d0499ef73402ad5fb25c20483b7495f20e291a1
BanseokSuh <still3028@gmail.com>
2024-04-07 12:20:36 +0900

    Refactor: Modify custom error code

--- src/main/java/com/banny/bannysns/exception/ErrorCode.java
+++ src/main/java/com/banny/bannysns/exception/ErrorCode.java
@@ -7,16 +7,18 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    OK(HttpStatus.OK, 2000), // "OK"
-    DUPLICATED_USER_ID(HttpStatus.CONFLICT, 4000), // "User ID is duplicated"
-    EMPTY_USER_ID(HttpStatus.BAD_REQUEST, 4001), // "User ID is empty"
-    INVALID_USER_ID_LENGTH(HttpStatus.BAD_REQUEST, 4002), // "User ID is too short"
-    EMPTY_USER_NAME(HttpStatus.BAD_REQUEST, 4003), // "Username is empty"
-    EMPTY_PASSWORD(HttpStatus.BAD_REQUEST, 4004), // "Password is blank"
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, 4005), // "Password is invalid"
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, 4006), // "User not found"
+    OK(HttpStatus.OK, 2000, "OK"),
+    DUPLICATED_USER_ID(HttpStatus.CONFLICT, 4000, "User ID is duplicated"),
+    EMPTY_USER_ID(HttpStatus.BAD_REQUEST, 4001, "User ID is empty"),
+    INVALID_USER_ID(HttpStatus.BAD_REQUEST, 4002, "User ID is invalid"),
+    EMPTY_USER_NAME(HttpStatus.BAD_REQUEST, 4003, "UserName is empty"),
+    EMPTY_PASSWORD(HttpStatus.BAD_REQUEST, 4004, "Password is empty"),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, 4005, "Password is invalid"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, 4006, "User not found"),
     ;
 
-    private HttpStatus httpStatus;
-    private Integer code;
+    private final HttpStatus httpStatus;
+    private final int code;
+    private final String message;
 }
+

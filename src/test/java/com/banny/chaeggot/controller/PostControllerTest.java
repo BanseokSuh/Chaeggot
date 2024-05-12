f38386690b725ad219d06cb91a09fff589e65402
BanseokSuh <still3028@gmail.com>
2024-05-12 09:29:46 +0900

    Feat: Add post-delete test code

--- src/test/java/com/banny/chaeggot/controller/PostControllerTest.java
+++ src/test/java/com/banny/chaeggot/controller/PostControllerTest.java
@@ -13,11 +13,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.banny.chaeggot.util.JwtTokenUtils.generateToken;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -61,8 +61,8 @@ public class PostControllerTest {
     }
 
     @Test
-    @DisplayName("게시글 작성 실패 - 인증되지 않은 사용자")
     @Transactional
+    @DisplayName("게시글 작성 실패 - 인증되지 않은 사용자")
     public void createPostFail() throws Exception {
         String title = "Test_title_001";
         String body = "Test_body_001";
@@ -75,8 +75,8 @@ public class PostControllerTest {
     }
 
     @Test
-    @DisplayName("게시글 수정")
     @Transactional
+    @DisplayName("게시글 수정")
     public void modifyPost() throws Exception {
         String title = "Modify_title_002";
         String body = "Modify_body_002";
@@ -93,8 +93,8 @@ public class PostControllerTest {
     }
 
     @Test
-    @DisplayName("게시글 수정 실패 - 인증되지 않은 사용자")
     @Transactional
+    @DisplayName("게시글 수정 실패 - 인증되지 않은 사용자")
     public void modifyPostFail() throws Exception {
         String title = "Modify_title_002";
         String body = "Modify_body_002";
@@ -107,8 +107,8 @@ public class PostControllerTest {
     }
 
     @Test
-    @DisplayName("게시글 수정 실패 - 다른 사용자의 게시글 수정 시도")
     @Transactional
+    @DisplayName("게시글 수정 실패 - 다른 사용자의 게시글 수정 시도")
     public void modifyPostFail2() throws Exception {
         String title = "Modify_title_002";
         String body = "Modify_body_002";
@@ -122,6 +122,21 @@ public class PostControllerTest {
                         .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body))))
                 .andDo(print())
                 .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("게시글 삭제")
+    public void deletePost() throws Exception {
+
+        User user = userService.loadUserByUserId("admin");
+        String token = generateToken(user, secretKey, accessExpiredTimeMs);
+
+        mockMvc.perform(delete("/api/v1/post/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + token))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
 }
